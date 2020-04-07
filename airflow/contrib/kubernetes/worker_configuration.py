23fed10e95163cdaea3c67613fcfc0b0694da023
Brandon T. Willard <971601+brandonwillard@users.noreply.github.com>
2020-04-07 15:35:09 -0500

    [AIRFLOW-6778] Add a configurable DAGs volume mount path for Kubernetes (#8147)

--- airflow/contrib/kubernetes/worker_configuration.py
+++ airflow/contrib/kubernetes/worker_configuration.py
@@ -369,6 +369,11 @@ class WorkerConfiguration(LoggingMixin):
         return volumes, volume_mounts
 
     def generate_dag_volume_mount_path(self):
+        """Generate path for DAG volume"""
+
+        if self.kube_config.dags_volume_mount_point:
+            return self.kube_config.dags_volume_mount_point
+
         if self.kube_config.dags_volume_claim or self.kube_config.dags_volume_host:
             dag_volume_mount_path = self.worker_airflow_dags
         else:
